4d7ad37d2528c9362b6819f3f07914afc5deb463
Ruichen Jiang <rayjiang30@outlook.com>
2023-10-23 12:13:25 -0500

    use seaborn for plotting

--- cubic_newton.py
+++ cubic_newton.py
@@ -11,13 +11,14 @@ from optimizer.LBFGS import Lbfgs
 from optimizer.GD import Gd, GD_LS
 from optimizer.reg_newton import RegNewton
 
+import seaborn as sns
 
 if __name__ == '__main__':
     # Define the loss function
     # dataset = 'gisette_scale'
     # dataset = 'madelon'
-    # dataset = 'w8a'
-    dataset = 'rcv1_train.binary'
+    dataset = 'w8a'
+    # dataset = 'rcv1_train.binary'
     # dataset = 'news20.binary'
     # if dataset == 'mushrooms':
     #     data_url = "https://www.csie.ntu.edu.tw/~cjlin/libsvmtools/datasets/binary/mushrooms"
@@ -34,19 +35,21 @@ if __name__ == '__main__':
     if not os.path.exists(data_path):
         f = urllib.request.urlretrieve(data_url, data_path)
     A, b = sklearn.datasets.load_svmlight_file(data_path)
+    A_csc = A.tocsc()
     # A = A.toarray()
 
     # Logistic regression problem
     loss = LogisticRegression(A, b, l1=0, l2=0, store_mat_vec_prod=True)
+    loss_csc = LogisticRegression(A_csc, b, l1=0, l2=0, store_mat_vec_prod=True)
     n, dim = A.shape
-    L = loss.smoothness
+    # L = loss.smoothness
     # l2 = 1e-10 * L # make the problem ill-conditioned
     # loss.l2 = l2
     x0 = np.ones(dim) * 0.5
 
-    flag_time = False # True for time, False for iteration
+    flag_time = True # True for time, False for iteration
     it_max = 50000
-    time_max = 30
+    time_max = 60
 
     # Define the optimization algs
     flag_LS = True # True for LS, False for fixed learning rate/regularization parameter
@@ -63,14 +66,14 @@ if __name__ == '__main__':
         reg_cub = np.geomspace(start=1e-8, stop=1e-2, num=7) * loss.hessian_lipschitz
     else:
         gd = GD_LS(loss=loss, label='GD LS')
-        cub_krylov = Cubic_Krylov_LS(loss=loss, reg_coef = 1, label='Cubic Newton LS (Krylov dim = {})'.format(memory_size),
+        cub_krylov = Cubic_Krylov_LS(loss=loss, reg_coef = 1e-3, label='Krylov CRN (m = {})'.format(memory_size),
                                subspace_dim=memory_size, tolerance = 1e-9)
         # cub_krylov = Cubic_Stoch_Krylov_LS(loss=loss, reg_coef = 1, label='Stochastic Cubic Newton LS (Krylov dim = {})'.format(memory_size),
         #                     subsampling=memory_size, 
         #                     subspace_dim=memory_size, tolerance = 1e-9)
         
-        cub_root = Cubic_LS(loss=loss, reg_coef = 1, label='Cubic Newton LS', tolerance = 1e-8)
-        sscn = SSCN(loss=loss, reg_coef = 1, label='SSCN (subspace dim = {})'.format(memory_size),
+        cub_root = Cubic_LS(loss=loss, reg_coef = 1e-3, label='CRN', cubic_solver="full", tolerance = 1e-8)
+        sscn = SSCN(loss=loss_csc, reg_coef = 1e-3, label='SSCN (m = {})'.format(memory_size),
                                subspace_dim=memory_size, tolerance = 1e-9)
 
     # A benchmark algorithm that is used to compute the optimal solution
@@ -95,10 +98,10 @@ if __name__ == '__main__':
     sscn.run(x0=x0, it_max=it_max, t_max=time_max)
     sscn.compute_loss_of_iterates()
 
-    # benchmark: SSCN
-    print(f'Running optimizer: {lbfgs.label}')
-    lbfgs.run(x0=x0, it_max=it_max, t_max=time_max)
-    lbfgs.compute_loss_of_iterates()
+    # # benchmark: SSCN
+    # print(f'Running optimizer: {lbfgs.label}')
+    # lbfgs.run(x0=x0, it_max=it_max, t_max=time_max)
+    # lbfgs.compute_loss_of_iterates()
 
     if not flag_LS:
         best_loss_gd = np.inf
@@ -121,6 +124,7 @@ if __name__ == '__main__':
         gd.from_pickle('./results/GD_best',loss=loss)
         # gd.trace.label = f'GD (lr={best_lr_gd})'
     else:
+        # pass
         print(f'Running optimizer: {gd.label}')
         gd.run(x0=x0, it_max=it_max, t_max=time_max)
         gd.compute_loss_of_iterates()
@@ -182,38 +186,62 @@ if __name__ == '__main__':
         cub_krylov.run(x0=x0, it_max=it_max, t_max=time_max)
         cub_krylov.compute_loss_of_iterates()
 
-        # print(f'Running optimizer: {cub_root.label}')
-        # cub_root.run(x0=x0, it_max=it_max, t_max=time_max)
-        # cub_root.compute_loss_of_iterates()
+        print(f'Running optimizer: {cub_root.label}')
+        cub_root.run(x0=x0, it_max=it_max, t_max=time_max)
+        cub_root.compute_loss_of_iterates()
 
 
 
 
     # Plot the loss curve
     # plt.style.use('tableau-colorblind10')
+    sns.set_style('ticks') # setting style
+    # sns.set_context('paper') # setting context
+    sns.set_palette('colorblind') # setting palette
 
+    color_cycle = plt.rcParams['axes.prop_cycle'].by_key()['color']
+
+    SMALL_SIZE = 10
+    MEDIUM_SIZE = 12
+    BIGGER_SIZE = 14
+
+    plt.rc('font', size=SMALL_SIZE)          # controls default text sizes
+    plt.rc('axes', titlesize=MEDIUM_SIZE)     # fontsize of the axes title
+    plt.rc('axes', labelsize=MEDIUM_SIZE)    # fontsize of the x and y labels
+    plt.rc('xtick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
+    plt.rc('ytick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
+    plt.rc('legend', fontsize=SMALL_SIZE)    # legend fontsize
+    plt.rc('figure', titlesize=BIGGER_SIZE)  # fontsize of the figure title
+
+    f_opt = min(loss.f_opt, loss_csc.f_opt)
     plt.style.use('seaborn-v0_8-colorblind')
-    gd.trace.plot_losses(marker='^', time=flag_time)
+    # gd.trace.plot_losses(marker='^', time=flag_time)
     # cub_krylov.trace.plot_losses(marker='>', label='cubic Newton (Krylov subspace)')
     # cub_root.trace.plot_losses(marker='o', label='cubic Newton (exact)')
 
-    cub_krylov.trace.plot_losses(marker='d', time=flag_time)
 
-    sscn.trace.plot_losses(marker='o', time=flag_time)
+    cub_root.trace.plot_losses(marker='o', markersize=5, f_opt=f_opt, time=flag_time)
 
-    # cub_root.trace.plot_losses(marker='s', time=flag_time)
+    sscn.trace.plot_losses(marker='^', markersize=6, f_opt=f_opt, time=flag_time)
 
-    lbfgs.trace.plot_losses(marker='X', time=flag_time)
+    cub_krylov.trace.plot_losses(marker='v', markersize=6, f_opt=f_opt, time=flag_time, color = color_cycle[7])
+
+
+
+    
+
+    # lbfgs.trace.plot_losses(marker='X', time=flag_time)
 
     # print(cub.trace.loss_vals)
     ## plt.xscale('log')
     if flag_time:
-        plt.xlabel('Time')
+        plt.xlabel('Time (s)')
     else:
         plt.xlabel('Iterations')
     plt.yscale('log')
     plt.legend()
     plt.grid()
+    plt.title('{} ($n={}$, $d={}$)'.format(dataset,n,dim))
     if flag_time:
         plt.savefig('figs/logistic_time_{}.pdf'.format(dataset))
     else:
