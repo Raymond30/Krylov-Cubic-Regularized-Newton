ded3bdacd4990ced60e37f632f81364af54f815e
Ruichen Jiang <rayjiang30@outlook.com>
2024-02-23 10:01:32 -0600

    change input arguments; multiple SSCN with different dims

--- cubic_newton.py
+++ cubic_newton.py
@@ -15,18 +15,25 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Cubic Regularized Newton Methods')
     parser.add_argument('--dataset', metavar='DATASETS', default='w8a', type=str,
                     help='The dataset')
-    parser.add_argument('--time', dest='flag_time', action='store_true',
+    parser.add_argument('--plot_time', dest='flag_time', action='store_true',
                     help='Plot with respect to time')
     parser.add_argument('--it_max', default=50000, type=int, metavar='IT',
                     help='max iteration')
     parser.add_argument('--time_max', default=60, type=float, metavar='T',
                     help='max time')
     
+    parser.add_argument('--SSCN_dim', nargs='+', default=10, type=int, metavar='D',
+                    help='Subspace dimensions in SSCN')
+    
     args = parser.parse_args()
     dataset = args.dataset
     flag_time = args.flag_time
     it_max = args.it_max
     time_max = args.time_max
+
+    m_list = args.SSCN_dim
+    if isinstance(m_list, int):
+        m_list = [m_list]
     # Define the loss function
     # dataset = 'gisette_scale'
     # dataset = 'madelon'
@@ -36,12 +43,15 @@ if __name__ == '__main__':
     data_url = "https://www.csie.ntu.edu.tw/~cjlin/libsvmtools/datasets/binary/{}".format(dataset)
 
     if dataset in {'gisette_scale','duke','rcv1_train.binary','news20.binary'}: # or dataset == 'epsilon_normalized':
+        data_url = '{}.bz2'.format(data_url)
         data_path = './{}.bz2'.format(dataset)
     else:
         data_path = './{}'.format(dataset)
     if not os.path.exists(data_path):
         f = urllib.request.urlretrieve(data_url, data_path)
     A, b = sklearn.datasets.load_svmlight_file(data_path)
+    
+    # Converting A into the Compressed Sparse Column (CSC) format; this is particularly suitable for SSCN
     A_csc = A.tocsc()
 
     # Define loss functions
@@ -56,33 +66,53 @@ if __name__ == '__main__':
 
     # Define the optimization algs
     memory_size = 10
+    memory_size_bench = 20
     cub_krylov = Cubic_Krylov_LS(loss=loss, reg_coef = 1e-3, label='Krylov CRN (m = {})'.format(memory_size),
                             subspace_dim=memory_size, tolerance = 1e-9)
     
-    cub_root = Cubic_LS(loss=loss, reg_coef = 1e-3, label='CRN', cubic_solver="full", tolerance = 1e-8)
-    sscn = SSCN(loss=loss_csc, reg_coef = 1e-3, label='SSCN (m = {})'.format(memory_size),
-                            subspace_dim=memory_size, tolerance = 1e-9)
+    # cub_krylov_bench is used to compute the optimal value of the function
+    cub_krylov_bench = Cubic_Krylov_LS(loss=loss, reg_coef= 1e-3, label='Benchmark Krylov CRN (m = {})'.format(memory_size_bench),
+                               subspace_dim=memory_size_bench, tolerance = 1e-9)
+    
+    if dim < 500:
+        cubic_solver = "full"
+    else:
+        cubic_solver = "CG"
+    cub_root = Cubic_LS(loss=loss, reg_coef = 1e-3, label='CRN', cubic_solver=cubic_solver, tolerance = 1e-8)
 
+    # sscn = SSCN(loss=loss_csc, reg_coef = 1e-3, label='SSCN (m = {})'.format(memory_size),
+    #                         subspace_dim=memory_size, tolerance = 1e-9)
+
+    sscn_list = [ ]
+    for m in m_list:
+        sscn_list.append(SSCN(loss=loss_csc, reg_coef = 1e-3, label='SSCN (m = {})'.format(m),
+                            subspace_dim=m, tolerance = 1e-9))
 
     
     # Running algs
 
+    
+
+    # Cubic regularized Newton
+    print(f'Running optimizer: {cub_root.label}')
+    cub_root.run(x0=x0, it_max=it_max, t_max=time_max)
+    cub_root.compute_loss_of_iterates()
+    time_max = max(cub_root.trace.ts[-1],time_max)
+
     # SSCN
-    print(f'Running optimizer: {sscn.label}')
-    sscn.run(x0=x0, it_max=it_max, t_max=time_max)
-    sscn.compute_loss_of_iterates()
+    for algs in sscn_list:
+        print(f'Running optimizer: {algs.label}')
+        algs.run(x0=x0, it_max=it_max, t_max=time_max)
+        algs.compute_loss_of_iterates()
 
-
+    # Krylov Cubic Regularized Newton
     print(f'Running optimizer: {cub_krylov.label}')
     cub_krylov.run(x0=x0, it_max=it_max, t_max=time_max)
     cub_krylov.compute_loss_of_iterates()
 
-    print(f'Running optimizer: {cub_root.label}')
-    cub_root.run(x0=x0, it_max=it_max, t_max=time_max)
-    cub_root.compute_loss_of_iterates()
-
-
-
+    print(f'Running optimizer: {cub_krylov_bench.label}')
+    cub_krylov_bench.run(x0=x0, it_max=5*it_max, t_max=5*time_max)
+    cub_krylov_bench.compute_loss_of_iterates()
 
     # Plot the loss curve
     # plt.style.use('tableau-colorblind10')
@@ -112,7 +142,8 @@ if __name__ == '__main__':
 
     cub_root.trace.plot_losses(marker='o', markersize=5, f_opt=f_opt, time=flag_time, label='CRN')
 
-    sscn.trace.plot_losses(marker='^', markersize=6, f_opt=f_opt, time=flag_time)
+    for algs in sscn_list:
+        algs.trace.plot_losses(marker='^', markersize=6, f_opt=f_opt, time=flag_time)
 
     cub_krylov.trace.plot_losses(marker='v', markersize=6, f_opt=f_opt, time=flag_time, color = color_cycle[7], label='Krylov CRN (m = 10)')
     if flag_time:
@@ -122,9 +153,13 @@ if __name__ == '__main__':
     plt.yscale('log')
     plt.legend()
     plt.grid()
-    plt.title('{} ($n={}$, $d={}$)'.format(dataset,n,dim))
+    plt.title('{} ($n={:,}$, $d={:,}$)'.format(dataset,n,dim))
+
+    if not os.path.exists('figs'):
+        os.makedirs('figs')
+
     if flag_time:
         plt.savefig('figs/time_{}.pdf'.format(dataset))
     else:
         plt.savefig('figs/iteration_{}.pdf'.format(dataset))
-    plt.show()
+    # plt.show()
